4a849e7d95830675ee8e251a1c4b944f94f9f0f2
cleissongomes <cleissongomes777@gmail.com>
2024-03-27 17:03:02 -0300

    feat(routes): add Delete method to accounts.js

--- routes/accounts.js
+++ routes/accounts.js
@@ -42,4 +42,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      account => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send({ message: 'A conta foi excluída com sucesso!' });
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
 export default router;
