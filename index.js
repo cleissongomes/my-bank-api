c22b4fa6ab01f9c323a8342fe42a28a347dc195c
cleissongomes <cleissongomes777@gmail.com>
2024-03-27 15:14:49 -0300

    feat(accounts): add POST route

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.use('/account', accountsRouter);
 
 app.listen(3000, async () => {
   try {
-    await readFile('account.json');
+    await readFile('accounts.json');
     console.log('API Started!');
   } catch (err) {
     const initialJson = {
       nextId: 1,
       accounts: [],
     };
-    writeFile('account.json', JSON.stringify(initialJson))
+    writeFile('accounts.json', JSON.stringify(initialJson))
       .then(() => {
         console.log('API Started and File Created!');
       })
