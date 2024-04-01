43bcbf84e49c1dbfdd03466a1429b3e577c61355
cleissongomes <cleissongomes777@gmail.com>
2024-04-01 10:24:25 -0300

    feat(docs): implement API documentation with Swagger

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import winston from 'winston';
 import cors from 'cors';
 import accountsRouter from './routes/accounts.js';
 import { promises as fs } from 'fs';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './doc.js';
 
 const { readFile, writeFile } = fs;
 
@@ -25,6 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/account', accountsRouter);
 
 app.listen(3000, async () => {
